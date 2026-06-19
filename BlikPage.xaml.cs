5d2c41f53499e897bbfc9439f97544232523b668
SokolskiDev <hashziom3k@tutanota.com>
2026-06-19 10:51:16 +0200

    next ui

--- BlikPage.xaml.cs
+++ BlikPage.xaml.cs
@@ -6,4 +6,23 @@ public partial class BlikPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void blik_copy_Clicked(object sender, EventArgs e)
+    {
+		Random r = new();
+		int blik = r.Next(999999);
+		int blik1 = blik / 1000;
+        int blik2 = blik % 1000;
+        string sblik1 = blik1.ToString();
+        string sblik2 = blik2.ToString();
+        if (blik1<100)
+			sblik1 = $"0{blik1}";
+		if (blik1<10)
+            sblik1 = $"00{blik1}";
+        if (blik2 < 100)
+            sblik2 = $"0{blik2}";
+        if (blik2 < 10)
+            sblik2 = $"00{blik2}";
+        blik_code.Text = $"{sblik1} {sblik2}";
+    }
 }
--- UserPage.xaml.cs
+++ UserPage.xaml.cs
@@ -2,18 +2,44 @@ namespace BankApp;
 
 public partial class UserPage : ContentPage
 {
-	public UserPage()
+    double saldo_konto = 100000.00;
+    double saldo_oszcz = 0.00;
+    double saldo_all = 0.00;
+    public UserPage()
 	{
 		InitializeComponent();
-	}
+        RefreshSaldo();
+
+    }
 
     private void btn_historia_Clicked(object sender, EventArgs e)
     {
+        saldo_oszcz += 1000;
+        RefreshSaldo();
 
     }
 
     private void btn_przelew_Clicked(object sender, EventArgs e)
     {
+        saldo_oszcz -= 1000;
+        RefreshSaldo();
+    }
 
+    private void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.Navigation.PushAsync(new MainPage());
+    }
+
+    //Blik_Button
+    private void ImageButton_Clicked_1(object sender, EventArgs e)
+    {
+        Shell.Current.Navigation.PushAsync(new BlikPage());
+    }
+    void RefreshSaldo()
+    {
+        saldo_all = saldo_konto + saldo_oszcz;
+        saldo_konto_amount.Text = $"{saldo_konto} PLN";
+        saldo_oszcz_amount.Text = $"{saldo_oszcz} PLN";
+        saldo_all_amount.Text = $"{saldo_all} PLN";
     }
 }
